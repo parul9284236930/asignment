418d4c16a9cead00475bb3cb0c6dc46573a9ce83
parul9284236930 <65996508+parul9284236930@users.noreply.github.com>
2022-03-04 19:57:46 +0530

    class work sprit example

---
+++ SprintExample/src/main/java/Address.java
@@ -0,0 +1,42 @@
+
+public class Address {
+public int dno;
+public Address();{
+}
+public int getDno() {
+	return dno;
+	
+}
+public void setDno(int dno) {
+	this.dno=dno;
+}
+
+
+
+public void setStreet(String street) {
+this.street = street;
+}
+
+ public String getCity() {
+return city;
+}
+
+ public void setCity(String city) {
+this.city = city;
+}
+
+ String street;
+String city;
+public Address(int dno, String street, String city) {
+super();
+this.dno = dno;
+this.street = street;
+this.city = city;
+}
+@Override
+public String toString() {
+return "Address [dno=" + dno + ", street=" + street + ", city=" + city + "]";
+}
+}
+
+}
